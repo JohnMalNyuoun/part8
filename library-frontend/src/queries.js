111fa65f09d8f95ebe4a92cdfd705e6303ac5512
Student <student@sirafrica.org>
2026-08-12 10:32:58 +0300

    Book view completed

--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client'
 
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`
+
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+      genres
+      id
+    }
+  }
+`
+
 export const ALL_DATA = gql`
   query {
     bookCount
